1ca7ca1eda1b296a8fd395f22e4a8323f255410f
Ansh443-21 <anahvaghela4@gmail.com>
2026-03-14 10:56:29 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,11 @@
-
+{
+    'name': 'Core Inventory',
+    'version': '1.0',
+    'summary': 'Simple Inventory Management System',
+    'author': 'Hackathon Team',
+    'category': 'Inventory',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
